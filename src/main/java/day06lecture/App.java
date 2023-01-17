6eadaf7088f60908c2c577e50caa2b3c4b8b16e0
Woody3797 <woody3797@gmail.com>
2023-01-17 11:31:09 +0800

    day6 threading using executorservice

--- src/main/java/day06lecture/App.java
+++ src/main/java/day06lecture/App.java
@@ -1,5 +1,8 @@
 package day06lecture;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  */
@@ -23,14 +26,34 @@ public final class App {
             }
             
         });
-        thread1.start();
+        //thread1.start();
 
-        MyRunnableImplementation mRI = new MyRunnableImplementation();
-        Thread thread2 = new Thread(mRI);
-        thread2.start();
+        MyRunnableImplementation mRI1 = new MyRunnableImplementation("task 1");
+        MyRunnableImplementation mRI2 = new MyRunnableImplementation("task 2");
+        MyRunnableImplementation mRI3 = new MyRunnableImplementation("task 3");
+        MyRunnableImplementation mRI4 = new MyRunnableImplementation("task 4");
+        MyRunnableImplementation mRI5 = new MyRunnableImplementation("task 5");
 
-        Thread thread3 = new Thread(mRI);
-        thread3.start();
+
+        // Thread thread2 = new Thread(mRI);
+        // thread2.start();
+
+        // Thread thread3 = new Thread(mRI);
+        // thread3.start();
+
+        // ExecutorService executorService = Executors.newSingleThreadExecutor();
+        // executorService.execute(mRI1);
+        // executorService.execute(mRI2);
+        // executorService.shutdown();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(mRI1);
+        executorService.execute(mRI2);
+        executorService.execute(mRI3);
+        executorService.execute(mRI4);
+        executorService.execute(mRI5);
+
+        executorService.shutdown();
 
     }
 }
